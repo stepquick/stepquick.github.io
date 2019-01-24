b6880963112c161e0c3ac49e0e29e8c63f428786
Quick <STEPHEN@dao.hctx.net>
2019-01-24 14:59:05 -0600

    Migrating to Gatsby 2.0 pt 2.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,23 @@
 module.exports = {
   siteMetadata: {
-  title: 'Stephen Quick',
-    author: 'Stephen Quick',
-    description: '',
-    siteUrl: 'https://stepquick.net',
+    title: `Stephen Quick`,
+    author: `Stephen Quick`,
+    description: `A web developer based in Houston, TX.`,
+    siteUrl: `https://stepquick.net/`
   },
-  pathPrefix: '/',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
       },
     },
     {
@@ -30,45 +36,58 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              // Class prefix for <pre> tags containing syntax highlighting;
-              // defaults to 'language-' (eg <pre class="language-js">).
-              // If your site loads Prism into the browser at runtime,
-              // (eg for use with libraries like react-live),
-              // you may use this to prevent Prism from re-processing syntax.
-              // This is an uncommon use-case though;
-              // If you're unsure, it's best to use the default value.
-              classPrefix: "language-",
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
-              inlineCodeMarker: null,
-              // This lets you set up language aliases.  For example,
-              // setting this to '{ sh: "bash" }' will let you use
-              // the language "sh" which will highlight using the
-              // bash highlighter.
-              aliases: {},
-            },
-          },
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
         ],
+      },
+    },
+    {
+      resolve: `gatsby-remark-prismjs`,
+      options: {
+        // Class prefix for <pre> tags containing syntax highlighting;
+        // defaults to 'language-' (eg <pre class="language-js">).
+        // If your site loads Prism into the browser at runtime,
+        // (eg for use with libraries like react-live),
+        // you may use this to prevent Prism from re-processing syntax.
+        // This is an uncommon use-case though;
+        // If you're unsure, it's best to use the default value.
+        classPrefix: "language-",
+        // This is used to allow setting a language for inline code
+        // (i.e. single backticks) by creating a separator.
+        // This separator is a string and will do no white-space
+        // stripping.
+        // A suggested value for English speakers is the non-ascii
+        // character '›'.
+        inlineCodeMarker: null,
+        // This lets you set up language aliases.  For example,
+        // setting this to '{ sh: "bash" }' will let you use
+        // the language "sh" which will highlight using the
+        // bash highlighter.
+        aliases: {},
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Stephen Quick's blog`,
+        short_name: `Stepquick`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/profile-pic.png`,
+      },
+    },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-plugin-typography',
+      resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: 'src/utils/typography',
+        pathToConfigModule: `src/utils/typography`,
       },
     },
   ],
