3cfa92110e5b0ef4b68eb864a332c69793de4792
Stephen Quick <2521852+stepquick@users.noreply.github.com>
2020-01-29 08:28:42 -0600

    Remove typography to cut down on how many resources browser needs to load.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,3 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
 require('prismjs/themes/prism-solarizedlight.css')
 require("./src/styles/styles.css");
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,59 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
+const typography = new Typography({
+  baseFontSize: "16px",
+  baseLineHeight: 1.5,
+  bodyFontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Fira Sans",
+    "Droid Sans",
+    "Helvetica Neue",
+    "sans-serif",
+  ],
+  scaleRatio: 2.25,
+  bodyWeight: 400,
+  headerWeight: 700,
+  headerFontFamily: ['Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  boldWeight: "bold",
+  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+    h1: scale(4 / 4),
+    h2: scale(3 / 4),
+    h3: scale(2 / 4),
+    h4: scale(1 / 6),
+    h5: scale(-1 / 6),
+    h6: scale(-2 / 6),
+    blockquote: {
+      ...scale(1 / 4),
+      borderLeft: `${rhythm(1 / 6)} solid #eceeef`,
+      paddingTop: rhythm(1 / 3),
+      paddingBottom: rhythm(1 / 3),
+      paddingLeft: rhythm(2 / 3),
+      paddingRight: rhythm(2 / 3),
     },
-  }
-}
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+    "blockquote > :last-child": {
+      marginBottom: 0,
+    },
+    "blockquote cite": {
+      ...adjustFontSizeTo(options.baseFontSize),
+      fontWeight: options.bodyWeight,
+      fontStyle: "normal",
+    },
+    a: {
+      boxShadow: "0 1px 0 0 currentColor",
+      color: "#007acc",
+      textDecoration: "none",
+    },
+    "a:hover,a:active": {
+      boxShadow: "none",
+    },
+  }),
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
