d6e19fccee096d2590f0e20f9443d77a37368944
Stephen Quick <2521852+stepquick@users.noreply.github.com>
2020-02-03 13:31:52 -0600

    add noreferrer for security

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -41,8 +41,8 @@ function Bio() {
               <p style={{marginBottom: rhythm(.25)}}>Written by <strong>{author}</strong>, a software developer based in Houston, Texas.</p>
               <p style={{display: `flex`, justifyContent: `flex-start`, flexFlow: "wrap"}}>
                 <a href="/resume" style={{marginRight:rhythm(1)}}>Resume.</a>
-                <a href="https://www.linkedin.com/in/stepquick" target="_blank" style={{marginRight:rhythm(1)}}>LinkedIn.</a>
-                <a href="https://github.com/stepquick" target="_blank">Github.</a>
+                <a href="https://www.linkedin.com/in/stepquick" target="_blank" rel="noreferrer" style={{marginRight:rhythm(1)}}>LinkedIn.</a>
+                <a href="https://github.com/stepquick" rel="noreferrer" target="_blank">Github.</a>
               </p>
             </div>
           </div>
