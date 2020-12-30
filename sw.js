4fd35160f43b1908817750ba81f9eafd1b33b7cc
Stephen Quick <2521852+stepquick@users.noreply.github.com>
2020-12-29 20:38:29 -0600

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a61e215e9a8cbf0e8201.js"
+    "url": "webpack-runtime-b2c5910285ce325034e6.js"
   },
   {
     "url": "styles.3a4099cb4dc837e07eff.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b552ad66dc499385532940151b6002da"
+    "revision": "3c175bc39b847b69fda37508ccdca347"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
